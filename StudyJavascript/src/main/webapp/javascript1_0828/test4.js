bb481bc744b59474bf9f27102cd05cfe669b0c48
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2023-09-11 15:43:47 +0900

    html 기본

--- StudyJavascript/src/main/webapp/javascript1_0828/test4.js
+++
@@ -1,8 +0,0 @@
-/*
-외부 자바스크립트 파일은 내부에<script> 태그를 기술하지 않음
-또한, 이 파일은 직접 실행되는 것이 아니라 다른 파일(HTML 문서) 내에 포함하여 사용됨
-*/
-alert("확인3");
-document.write("<hr>");
-document.write("<h1>이 곳은 외부 자바스크립트 파일입니다.</h1>");
-document.write("<hr>");
--- StudyJavascript/src/main/webapp/javascript3_0904_function/test13.js
+++
@@ -1,7 +0,0 @@
-/**
- * 함수를 갖는 외부 자바스크립트 파일
- */
-// "메세지" 라는 문자열을 출력하는 showMessage() 함수 정의
-function showMessage() {
-	document.write("메세지<br>");
-}
