19d52ddeab89b39a61a1aa5395df3987b31df241
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2023-09-11 15:44:44 +0900

    09.11

---
+++ StudyJavascript/src/main/webapp/javascript3_0905_function/test15.js
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+// 외부로부터 문자열 1개를 전달받아 해당 문자열을 출력하는 showMessage() 함수 정의
+// => 전달받을 문자열을 저장하는 매개변수 msg 기술
+function showMessage(msg) {
+	document.write(msg + "<br>"); // 전달받은 문자열 출력
+}
